d98657ae8923d7d36a0d51707c5db57484770792
unknown <Premjithprabhakar>
2022-11-18 23:08:02 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefualtComponent } from './layouts/defualt/defualt.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { DefualtModule } from './layouts/defualt/defualt.module';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { PostsComponent } from './modules/posts/posts.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DefualtModule
   ],
   providers: [],
   bootstrap: [AppComponent]
